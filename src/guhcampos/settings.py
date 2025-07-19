b4073ed0949ae91a09a89c6cad3d1a3ab5135b4f
Gustavo Campos <guhcampos@users.noreply.github.com>
2025-07-19 12:39:53 -0300

    publish a couple more playlists, add playlist sorting

--- src/guhcampos/settings.py
+++ src/guhcampos/settings.py
@@ -25,19 +25,32 @@ class Settings(BaseSettings):
     spotify_user_id: str
     spotify_playlists: list[str] = [
         "This is Guhcampos",
-        "80's Stuff the Nice and the Ugly",
-        "Beard Growing",
-        "Banda Indie Canta Hey!",
-        "Breathe",
-        "Dirty Rockin",
-        "Esquerda Festiva",
-        "Everybody Loves Covers",
-        "Funksouzera",
-        "Hot and Hard",
-        "Metalheads in Love",
-        "Powerzera",
-        "Rebolation",
-        "Smooth Talkin",
+        "Beard Growing",  # 01.01
+        "Breathe",  # 01.02
+        "Rock de Adulto",  # 01.03
+        "Banda Indie Canta Hey!",  # 01.04
+        # "Lento, Sujo e Pesado", # 01.05
+        # Worshipping Questionable Entities", # 01.06
+        "Prog Jovem",  # 01.07,
+        "Funksouzera",  # 01.08
+        "Dirty Rockin",  # 01.09
+        "Esquerda Festiva",  # 01.10
+        "Rebolation",  # 01.11
+        # Metalerage", # 01.12
+        "Smooth Talkin",  # 01.13
+        "Powerzera",  # 01.14
+        "Hot and Hard",  # 01.15
+        # "Weird Stuff", # 01.16
+        "80's Stuff the Nice and the Ugly",  # 01.17
+        "Metalheads in Love",  # 01.18
+        "Everybody Loves Covers",  # 01.19
+        # "Game Soundtracks", # 01.20,
+        "Balada G0y Internacional",  # 01.21,
+        "Party Rock",  # 01.22,
+        # "90's Tash Awesome Shit Great", # 01.23,
+        "Resenha Fortíssima",  # 01.24,
+        "Metalerage Corset",  # 01.25,
+        "Heavy Brazilian Fucking Metal",  # 01.26
     ]
     spotify_playlists_output_dir: Path = REPO_ROOT / "hugo" / "static" / "playlists"
 
